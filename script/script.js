abd856b4e35c3e321b23c89abf69868248a0ab0f
Moscardoqm <91522989+Moscardoqm@users.noreply.github.com>
2022-03-14 22:47:32 +0100

    Add files via upload

---
+++ script/script.js
@@ -0,0 +1,7 @@
+document.getElementsByClassName("subtitul").addEventListener("mouseover",function(){
+    document.getElementsByClassName("subtitul").style.backgroundColor="green";
+});
+
+document.getElementsByClassName("subtitul").addEventListener("mouseout",function(){
+    document.getElementsByClassName("subtitul").style.backgroundColor="red";
+});
